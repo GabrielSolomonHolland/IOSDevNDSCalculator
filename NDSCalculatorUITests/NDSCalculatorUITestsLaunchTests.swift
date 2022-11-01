1460bd5711c329dd6db388b628a164f67e28b799
Gabriel Solomon Holland <gabrielsolomonholland@gmail.com>
2022-11-01 08:15:21 -0500

    Initial Commit

---
+++ NDSCalculatorUITests/NDSCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NDSCalculatorUITestsLaunchTests.swift
+//  NDSCalculatorUITests
+//
+//  Created by Solomon-Holland,Gabriel I on 11/1/22.
+//
+
+import XCTest
+
+class NDSCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
